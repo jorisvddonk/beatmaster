d791799c4f5d6bc66a854297da766883ae5ce59c
joris <jorisvddonk@gmail.com>
2018-05-19 20:40:16 +0200

    Load track json from zip file and prepare to display it

--- index.js
+++ index.js
@@ -5,26 +5,46 @@ require('./shaders/skyGradient.js');
 
 var songParser = fetch('http://127.0.0.1:8081/data/song.zip').then(function(x){return JSZip.loadAsync(x.blob())});
 
+var setTitle = function(title){
+    document.getElementById('song_title').setAttribute('text', 'value', title.toString());
+};
+var showError = function(err){
+    console.error(err);
+    setTitle(err);
+};
+
 var getFileInZip = function(jszip, filename) {
     return new Promise(function(resolve, reject){
         for (var i in jszip.files) {
             var file = jszip.files[i];
-            if (file.name.split('/').pop().toLowerCase() === filename) {
+            if (file.name.split('/').pop().toLowerCase() === filename.toLowerCase()) {
                 resolve(file);
                 break;
             }
         }
         reject('file not found');
     });
-}
+};
+
+var displayTrack = function(trackdetails) {
+    console.log(trackdetails); //todo
+};
 
 songParser.then(function(jszip){
     getFileInZip(jszip, 'info.json').then(function(file){
-        file.async('text').then(function(filedetails){
+        return file.async('text').then(function(filedetails){
             var filejson = JSON.parse(filedetails);
-            console.log(filejson);
             var title = `${filejson.songName} - ${filejson.songSubName} (by ${filejson.authorName})`;
-            document.getElementById('song_title').setAttribute('text', 'value', title);
+            setTitle(title);
+            console.log(filejson)
+            var trackjsonfilename = filejson.difficultyLevels[filejson.difficultyLevels.length-1].jsonPath;
+            console.log("Loading... " + trackjsonfilename);
+            getFileInZip(jszip, trackjsonfilename).then(function(trackjsonfile){
+                return trackjsonfile.async('text').then(function(filedetails){
+                    var trackjson = JSON.parse(filedetails);
+                    displayTrack(trackjson);
+                });
+            }).catch(showError);
         });
-    });
-});
+    }).catch(showError);
+}).catch(showError);
