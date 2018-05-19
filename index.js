ad414bc4120d6b5704a840081fbe6602c7ba0877
joris <jorisvddonk@gmail.com>
2018-05-19 20:26:13 +0200

    refactor

--- index.js
+++ index.js
@@ -5,15 +5,26 @@ require('./shaders/skyGradient.js');
 
 var songParser = fetch('http://127.0.0.1:8081/data/song.zip').then(function(x){return JSZip.loadAsync(x.blob())});
 
-songParser.then(function(jszip){
-    for (var i in jszip.files) {
-        var file = jszip.files[i];
-        if (file.name.split('/').pop().toLowerCase() === 'info.json') {
-            file.async('text').then(function(filedetails){
-                var filejson = JSON.parse(filedetails);
-                var title = `${filejson.songName} - ${filejson.songSubName} (by ${filejson.authorName})`;
-                document.getElementById('song_title').setAttribute('text', 'value', title);
-            });
+var getFileInZip = function(jszip, filename) {
+    return new Promise(function(resolve, reject){
+        for (var i in jszip.files) {
+            var file = jszip.files[i];
+            if (file.name.split('/').pop().toLowerCase() === filename) {
+                resolve(file);
+                break;
+            }
         }
-    };
+        reject('file not found');
+    });
+}
+
+songParser.then(function(jszip){
+    getFileInZip(jszip, 'info.json').then(function(file){
+        file.async('text').then(function(filedetails){
+            var filejson = JSON.parse(filedetails);
+            console.log(filejson);
+            var title = `${filejson.songName} - ${filejson.songSubName} (by ${filejson.authorName})`;
+            document.getElementById('song_title').setAttribute('text', 'value', title);
+        });
+    });
 });
