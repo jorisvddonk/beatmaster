edd69f9e495af123dedd90bb995b0f6f8b4bb267
joris <jorisvddonk@gmail.com>
2018-05-19 23:05:49 +0200

    Show hand location (as orbs) if available

--- index.js
+++ index.js
@@ -84,6 +84,18 @@ AFRAME.registerComponent('game-track', {
     }
 });
 
+AFRAME.registerComponent('remove-hand-controls', {
+    init: function() {},
+    tick: function(time) {
+        // remove default hand model
+        var obj3d = this.el.getObject3D('mesh');
+        if (obj3d) {
+            this.el.removeObject3D('mesh');
+            this.el.removeAttribute('remove-hand-controls'); // once default hand model is removed; remove this component
+        }
+    }
+});
+
 songParser.then(function(jszip){
     getFileInZip(jszip, 'info.json').then(function(file){
         return file.async('text').then(function(filedetails){
